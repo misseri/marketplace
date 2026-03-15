8a1cfd62d50441eafb8fd65efbe299d4dc72af74
misseri <nutado2005@gmail.com>
2026-03-15 20:54:36 +0600

    Add frontend part

--- src/app/components/MainPageHeader/page.tsx
+++ src/app/components/MainPageHeader/page.tsx
@@ -29,20 +29,59 @@ import {
   DialogTrigger,
 } from "~/components/ui/dialog";
 import GoogleSVG from "./img/GoogleSVG";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Header() {
   const [isLogged, setIsLogged] = useState<boolean>(false);
   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  const handleLogging = () => {
-    setIsLogged(true);
-    setIsDialogOpen(false);
+  async function checkAuth() {
+    const whoAmIUrl = "http://localhost:8080/auth/whoami";
+    try {
+      const response = await fetch(whoAmIUrl, {
+        credentials: "include",
+        mode: "cors",
+      });
+      console.log("Status:", response.status);
+      if (!response.ok) {
+        setIsLogged(false);
+        return;
+      }
+      const userData = await response.json();
+      console.log("LOGGED:", userData.login);
+      setIsLogged(true);
+    } catch (error) {
+      console.error("Not logged:", error);
+      setIsLogged(false);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  function handleLogging() {
+    window.location.href = "http://localhost:8080/oauth2/authorization/google";
+  }
+
+  const handleLogout = async () => {
+    try {
+      await fetch("http://localhost:8080/auth/logout", {
+        method: "POST",
+        credentials: "include",
+      });
+    } catch {
+    } finally {
+      setIsLogged(false);
+    }
   };
 
-  const handleLogout = () => {
-    setIsLogged(false);
-  };
+  if (loading) {
+    return <header>Проверка авторизации...</header>;
+  }
 
   return (
     <header className="mx-4 flex flex-wrap items-center justify-between gap-3 border-b-2 border-gray-400 py-3 sm:mx-8 sm:gap-4 sm:py-4 md:mx-12 md:gap-4.5 md:py-4.5 lg:mx-16 xl:mx-20">
@@ -61,6 +100,7 @@ export default function Header() {
           />
         </div>
       </div>
+
       <nav className="order-2 flex-shrink-0 sm:order-3">
         <ul className="flex items-center justify-between gap-4 text-[#F62877] sm:gap-6 md:gap-8 lg:gap-9.5">
           <li className="h-6 w-6 sm:h-7 sm:w-7 md:h-[30px] md:w-[30px]">
@@ -91,32 +131,22 @@ export default function Header() {
                 >
                   <DropdownMenuGroup>
                     <DropdownMenuItem>
-                      <CircleUser />
-                      Профиль
-                      {/* i18n интернационализация */}
+                      <CircleUser /> Профиль
                     </DropdownMenuItem>
                     <DropdownMenuItem>
-                      <ShoppingBasket />
-                      Корзина
-                      {/* i18n интернационализация */}
+                      <ShoppingBasket /> Корзина
                     </DropdownMenuItem>
                     <DropdownMenuItem>
-                      <Heart />
-                      Избранное
-                      {/* i18n интернационализация */}
+                      <Heart /> Избранное
                     </DropdownMenuItem>
                     <DropdownMenuItem>
-                      <Settings />
-                      Настройки
-                      {/* i18n интернационализация */}
+                      <Settings /> Настройки
                     </DropdownMenuItem>
                   </DropdownMenuGroup>
                   <DropdownMenuSeparator />
                   <DropdownMenuGroup>
                     <DropdownMenuItem onClick={handleLogout}>
-                      <LogOut />
-                      Выйти
-                      {/* i18n интернационализация */}
+                      <LogOut /> Выйти
                     </DropdownMenuItem>
                   </DropdownMenuGroup>
                 </DropdownMenuContent>
@@ -134,7 +164,6 @@ export default function Header() {
                       <DialogTitle className="text-2xl text-[#F62877]">
                         Вход
                       </DialogTitle>
-                      {/* mock заглушку сделать */}
                     </DialogHeader>
                     <button
                       onClick={handleLogging}
