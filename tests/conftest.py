6d280a483dec74241cb2544028e6c68916f3c615
iivanov627 <tselikov_2004@mail.ru>
2026-04-26 11:35:45 +0600

    add tests

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+def pytest_collection_modifyitems(items):
+    for item in items:
+        if item.get_closest_marker("positive"):
+            item._nodeid = f"{item.nodeid} [POSITIVE]"
+        elif item.get_closest_marker("negative"):
+            item._nodeid = f"{item.nodeid} [NEGATIVE]"
